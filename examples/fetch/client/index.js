826820d4e602a3d180f2143c28c3d09e7fb7373e
dcs0spb <s.p.bradley@durham.ac.uk>
2025-02-05 12:25:35 +0000

    Using async/await, exceptions and checking response for 404

--- examples/fetch/client/index.js
+++ examples/fetch/client/index.js
@@ -1,7 +1,14 @@
-window.addEventListener('click', function(event){
-  fetch('http://127.0.0.1:8080/list')
-   .then(response => response.text())
-   .then(body =>
-      document.getElementById('content').innerHTML=body)
-    .catch( (error) => alert(error))
+window.addEventListener('click', async function(event){
+  try{
+    let response = await fetch('http://127.0.0.1:8090/listy')
+    if(response.ok){
+      let body = await response.text ()
+      document.getElementById('content').innerHTML=body
+    }
+    else{
+      this.alert("you are a fool you have got a 404 try learning to type")
+    }
+  } catch(e) {
+    alert(e)
+  }
 });
