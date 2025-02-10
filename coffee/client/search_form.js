c66ef9c1c2783e61a1b447ff3a0a6d13afd55a84
dcs0spb <s.p.bradley@durham.ac.uk>
2025-02-10 13:38:02 +0000

    extracting value from search form

--- coffee/client/search_form.js
+++ coffee/client/search_form.js
@@ -1,3 +1,9 @@
-const search_button = document.getElementById("search_button")
+const search_button = document.getElementById("extract_data")
 
-search_button.addEventListener('click', () => alert("button clicked"))
+const search_form = document.getElementById('search_form')
+search_form.addEventListener('submit', function(){
+    let input_elt = document.getElementById('search_term')
+    let search_term = input_elt.value
+    alert("button clicked " + input_elt.value)
+
+})
