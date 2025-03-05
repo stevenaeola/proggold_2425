475c454fdddc7fee7eccf0328670f764fca71eab
dcs0spb <s.p.bradley@durham.ac.uk>
2025-03-05 12:27:28 +0000

    Notify user on error. Closes #6

--- coffee/client/add_form.js
+++ coffee/client/add_form.js
@@ -1,18 +1,22 @@
 
 const add_form = document.getElementById('add_form')
 add_form.addEventListener('submit',  async function(event){
-    alert('handling form submission')
     event.preventDefault()
 
     const formData = new FormData(add_form);
     const formJSON = JSON.stringify(Object.fromEntries(formData.entries()));
- 
-    const response = await fetch('/coffee/add',
+    try
         {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-              },
-            body: formJSON
-        });
+            const response = await fetch('/coffee/add',
+                {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: formJSON
+                });
+        }
+    catch(e){
+        alert(e)
+    }
 })
