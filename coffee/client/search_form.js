f7b9cf4467d80b753740528a3f5c0a920eb00d94
dcs0spb <s.p.bradley@durham.ac.uk>
2025-03-12 12:54:38 +0000

    Get details from clicking on search results. Closes #15

--- coffee/client/search_form.js
+++ coffee/client/search_form.js
@@ -13,7 +13,31 @@ search_form.addEventListener('keyup',  async function(event){
     let results_list = document.getElementById('search_results')
     results_list.innerHTML = ""
     for(let result of results){
-        results_list.innerHTML += "<li>" + result.name + " </li>"
+        results_list.innerHTML += 
+        `<li><a href='/coffee/details/${result.name}'>${result.name}</a> </li>`
+    }
+    let links = document.querySelectorAll("#search_results > li > a")
+    for(let link of links){
+        link.addEventListener('click', async function(event){
+            event.preventDefault()
+            let request_url = event.target.getAttribute("href")
+            let response = await fetch(request_url)
+            let body = await response.text()
+            let coffee = JSON.parse(body)
+            const details_results = document.getElementById("detail_results")
+            details_results.innerHTML = 
+            `
+            <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${coffee.name}</h5>
+    <p class="card-text">Strength: ${coffee.strength}</p>
+    <p class="card-text">Taste: ${coffee.taste}</p>
+
+  
+  </div>
+</div>
+`
+        })
     }
 
 })
